dbcc4b5deeda5f905fe12dac5bd800c620bb8422
techninja <james@tn42.com>
2015-05-12 14:13:16 -0700

    Heavily refactor code to allow robust reconnection The littlebits power connector or USB cable is easily separated by little hands, killing the Arduino :P

--- etch-a-bits.js
+++ etch-a-bits.js
@@ -1,8 +1,26 @@
 var cncserver = require("cncserver");
 var serialport = require("./node_modules/cncserver/node_modules/serialport/serialport.js");
 var SerialPort = serialport.SerialPort;
+var serialPort = {};
 
 var port = process.argv[2];
+
+var x = 0;
+var y = 0;
+var b = 0;
+
+var lastB = 0;
+var lastX = 0;
+var lastY = 0;
+
+var gettingWater = false;
+var movingPen = false;
+
+var doubleClickWait = false;
+var reWaterInterval = 0;
+
+var drawPt = {x: 0, y: 0};
+
 
 // Actually try to init the connection and handle the various callbacks
 cncserver.start({
@@ -25,92 +43,83 @@ cncserver.start({
 });
 
 function startReading(){
-  var x = 0;
-  var y = 0;
-  var b = 0;
-
-  var lastB = 0;
-  var lastX = 0;
-  var lastY = 0;
-
-  var gettingWater = false;
-  var movingPen = false;
-
-  var doubleClickWait = false;
-  var reWaterInterval = 0;
-
-  var drawPt = {x: 0, y: 0};
-
-  var serialPort = new SerialPort(port, {
+  serialPort = new SerialPort(port, {
     baudrate : 9600,
-    parser: serialport.parsers.readline("\n")
+    parser: serialport.parsers.readline("\n"),
+    disconnectedCallback: tryReconnect
   });
 
-  serialPort.on("data", function(data){
-    var d = data.split('|');
-    x = parseInt(d[0]);
-    y = parseInt(d[1]);
-    b = parseInt(d[2]);
+  serialPort.on("open", function(){
+    console.log('Open!');
+    serialPort.on("data", controlData);
+  });
+}
 
-    // Button get water
-    if (b!== lastB) {
-      lastB = b;
-      if (b === 0) {
-        if (!gettingWater) {
-          if (!doubleClickWait) {
-            doubleClickWait = true;
-            console.log('Single button...');
-            reWaterInterval = setTimeout(function(){
-              doubleClickWait = false;
-              console.log('Button Pressed/Released! getting Water');
-              getWater(drawPt);
-            }, 250);
-          } else {
-            console.log('Double button! Parking.');
-            clearTimeout(reWaterInterval);
-            movingPen = true;
+
+// Event data callback from littleBits Leonardo
+function controlData (data) {
+  var d = data.split('|');
+  x = parseInt(d[0]);
+  y = parseInt(d[1]);
+  b = parseInt(d[2]);
+
+  // Button get water
+  if (b!== lastB) {
+    lastB = b;
+    if (b === 0) {
+      if (!gettingWater) {
+        if (!doubleClickWait) {
+          doubleClickWait = true;
+          console.log('Single button...');
+          reWaterInterval = setTimeout(function(){
             doubleClickWait = false;
-            cncserver.setHeight('up');
-            cncserver.setPen({x: 0, y: 0, park: true}, function(){
-              movingPen = false;
-            });
-          }
-
+            console.log('Button Pressed/Released! getting Water');
+            getWater(drawPt);
+          }, 250);
         } else {
-          console.log('Already Getting Water');
+          console.log('Double button! Parking.');
+          clearTimeout(reWaterInterval);
+          movingPen = true;
+          doubleClickWait = false;
+          cncserver.setHeight('up');
+          cncserver.setPen({x: 0, y: 0, park: true}, function(){
+            movingPen = false;
+          });
         }
+
+      } else {
+        console.log('Already Getting Water');
       }
     }
+  }
 
-    // X/Y Movement
-    if (!gettingWater && !movingPen) {
-      drawPt = {
-        x: (lastX + 1) / 10.24,
-        y: (lastY + 1) / 10.24
-      };
+  // X/Y Movement
+  if (!gettingWater && !movingPen) {
+    drawPt = {
+      x: (lastX + 1) / 10.24,
+      y: (lastY + 1) / 10.24
+    };
 
-      if (x!== lastX && x!== lastX-1 && x!== lastX+1) {
-        lastX = x;
-        drawPt.x = (x+1) / 10.24;
-        movingPen = true;
-      }
-
-      if (y!== lastY && y!== lastY-1 && y!== lastY+1) {
-        lastY = y;
-        drawPt.y = (y+1) / 10.24;
-        movingPen = true;
-      }
-
-      // If this is set, then we had a change enough to set this
-      if (movingPen) {
-        //console.log(drawPt);
-        cncserver.setPen(drawPt, function(){
-          movingPen = false;
-        });
-      }
+    if (x!== lastX && x!== lastX-1 && x!== lastX+1) {
+      lastX = x;
+      drawPt.x = (x+1) / 10.24;
+      movingPen = true;
     }
 
-  });
+    if (y!== lastY && y!== lastY-1 && y!== lastY+1) {
+      lastY = y;
+      drawPt.y = (y+1) / 10.24;
+      movingPen = true;
+    }
+
+    // If this is set, then we had a change enough to set this
+    if (movingPen) {
+      //console.log(drawPt);
+      cncserver.setPen(drawPt, function(){
+        movingPen = false;
+      });
+    }
+  }
 }
 
 // Wrapper for getting water
@@ -144,4 +153,27 @@ function connectLeonardo(callback) {
       callback();
     }
   });
+}
+
+// Trys again and again to reconnect, every 2 seconds
+function tryReconnect() {
+  console.log('Arduino Disconnected! Trying to reconnect...');
+  var tryReconnect = setInterval(function(){
+    try {
+      serialPort.open(function(){
+        if (serialPort.isOpen()) {
+          clearInterval(tryReconnect);
+          console.log('Reconnect succeeded!');
+
+          // Data callback needs to be rebound... for some reason :P
+          serialPort.on("data", controlData);
+        } else {
+          console.log('Connect fail...');
+        }
+      });
+    } catch(e) {
+      // Didn't reconnect
+      console.log('Reconnect failed, trying again...', e);
+    }
+  }, 2000);
 }
